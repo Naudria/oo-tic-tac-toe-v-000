3a77fac13901c31fbff4f19344e0e1731f728c48
Rima Jean <rima.jean@gmail.com>
2018-02-12 19:01:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,9 +44,7 @@ WIN_COMBINATIONS = [
    def turn_count
        @board.count{|token| token == "X" || token == "O"}
      end
-   
+
   def current_player
       turn_count % 2 == 0 ? "X" : "O"
     end
-
-
